cf556e60dece9951b12f2f80e3ad4623cb8ed81c
Enrico Franchi <efranchi@ce.unipr.it>
2011-08-30 10:39:22 +0200

    First commit

---
+++ src/it/unipr/aotlab/dsns/DSNS.java
@@ -0,0 +1,25 @@
+/* Copyright */
+
+package it.unipr.aotlab.dsns;
+
+import org.gudy.azureus2.plugins.PluginException;
+import org.gudy.azureus2.plugins.PluginInterface;
+import org.gudy.azureus2.plugins.UnloadablePlugin;
+
+/**
+ * User: enrico
+ * Package: it.unipr.aotlab.dsns
+ * Date: 8/29/11
+ * Time: 10:29 AM
+ */
+public class DSNS implements UnloadablePlugin {
+    @Override
+    public void unload() throws PluginException {
+
+    }
+
+    @Override
+    public void initialize(final PluginInterface pluginInterface) throws PluginException {
+
+    }
+}
